9de7fd044ed12180aa56855585e4a281166692ff
bhawesh <rautelabhawesh44@gmail.com>
2025-10-15 18:16:48 +0530

    Add files via upload

---
+++ Experiment1/address.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Printing address in multiple lines
+    printf("Bhawesh Rautela\n");
+    printf("UPES Dehradun\n");
+    printf("Bidholi Campus\n");
+    printf("Dehradun, Uttarakhand\n");
+    printf("PIN - 248007\n");
+
+    return 0;
+}
